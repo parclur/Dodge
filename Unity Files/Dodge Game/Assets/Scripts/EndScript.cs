2c36907753e1506b2105a8c05e5c503160009b58
claire.yeash <claire.yeash@e1f550a6-085a-433b-8d19-7e151ec04d88>
2018-04-16 15:00:48 +0000

    Some files that I forgot to add

---
+++ Unity Files/Dodge Game/Assets/Scripts/EndScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndScript : MonoBehaviour {
+
+	public void RestartGame()
+    {
+        SceneManager.LoadScene("Start_Menu");
+    }
+}
---
+++ Unity Files/Dodge Game/Assets/Scripts/StartScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Player_Login_Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
