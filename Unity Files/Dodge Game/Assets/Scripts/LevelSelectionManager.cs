e080b6499f954da21ea659e0c8ab3a6992f7ff41
claire.yeash <claire.yeash@e1f550a6-085a-433b-8d19-7e151ec04d88>
2018-04-26 14:04:36 +0000

    Finished the menus

---
+++ Unity Files/Dodge Game/Assets/Scripts/LevelSelectionManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionManager : MonoBehaviour {
+
+    public GameObject levelSelectionCanvas;
+    public GameObject roundSelectionCanvas;
+
+    public static int numberOfRounds = 1;
+    public Text numberOfRoundsText;
+
+    public GameObject level1Button;
+    public UnityEngine.UI.Button level1SelectionButton;
+
+    void Start()
+    {
+        levelSelectionCanvas.SetActive(false);
+        roundSelectionCanvas.SetActive(true);
+    }
+
+    void Update()
+    {
+        numberOfRoundsText.text = numberOfRounds.ToString();
+    }
+
+    public void MoreRounds()
+    {
+        numberOfRounds++;
+
+        if (numberOfRounds > 15)
+        {
+            numberOfRounds = 1;
+        }
+    }
+
+    public void LessRounds()
+    {
+        numberOfRounds--;
+
+        if (numberOfRounds < 1)
+        {
+            numberOfRounds = 15;
+        }
+    }
+
+    public void Next()
+    {
+        levelSelectionCanvas.SetActive(true);
+        roundSelectionCanvas.SetActive(false);
+        level1SelectionButton.Select();
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Test_Level_2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Test_Level_3");
+    }
+}
